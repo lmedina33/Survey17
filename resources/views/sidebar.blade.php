251a2a723ab1b98e57770acaada176f6fcba3bdc
unknown <luismedina3@hotmail.com>
2017-07-26 13:28:53 -0500

    principal1

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,9 @@
+  <!-- sidebar nav -->
+    <nav id="sidebar-nav">
+        <ul class="nav nav-pills nav-stacked">
+            <li><a href="#">Datos Generales</a></li>
+            <li><a href="#">Módulo 1</a></li>
+            <li><a href="#">Módulo 2</a></li>
+            <li><a href="#">Módulo 3</a></li>
+        </ul>
+    </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,94 +1,34 @@
 <!DOCTYPE html>
-<html lang="en-us">
-        <head>
-                <meta charset="utf-8">
-                <title>Encuestas -  CGR - SCI - 2017</title>
-                <meta name="description" content="Live preview for Material Style - Material Design For All at WrapBootstrap">
-                <meta name="viewport" content="width=1024">
-                <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-                <link rel="stylesheet" type="text/css" media="screen" href="//d85wutc1n854v.cloudfront.net/live/css/screen_preview_legacy.css">
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-                <!--<script src="//cdn.optimizely.com/js/233905874.js"></script>-->
-                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-                 <link rel="stylesheet" href="https://agmstudio.io/themes/material-style/1.5.1/assets/css/style.light-blue-500.min.css" />
-                 <link rel="stylesheet" href="https://agmstudio.io/themes/material-style/1.5.1/assets/css/plugins.min.css" />
-                 <link rel="stylesheet" href="https://agmstudio.io/themes/material-style/1.5.1/assets/css/preload.min.css" />
-                 <link rel="stylesheet" href="https://agmstudio.io/themes/material-style/1.5.1/assets/css/width-boxed.min.css" id="ms-boxed" disabled="">
-                <script src="//d85wutc1n854v.cloudfront.net/live/js/behavior_legacy.js"></script>
-                <!--[if lt IE 9]>
-        <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/js/html5shiv.min.js"></script>
-        <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/js/respond.min.js"></script>
-    <![endif]-->
-                
-                <link rel="shortcut icon" href="//d85wutc1n854v.cloudfront.net/live/imgs/favicon.ico">
-        </head>
-        <body>
-                <div id="main">
-                        <header class="ms-header ms-header-primary">
-                                <div class="container container-full">
-          <div class="ms-title">
-            <a href="index.html">
-              <!-- <img src="assets/img/demo/logo-header.png" alt=""> -->
-              <span class="ms-logo animated zoomInDown animation-delay-5">E</span>
-              <h1 class="animated fadeInRight animation-delay-6">Encuesta -
-                <span>CGR-SCI-Comunicaciones</span>
-              </h1>
-            </a>
-          </div>
-          <div class="header-right">
-            <div class="share-menu">
-              <ul class="share-menu-list">
-                <li class="animated fadeInRight animation-delay-3">
-                  <a href="javascript:void(0)" class="btn-circle btn-google">
-                    <i class="zmdi zmdi-google"></i>
-                  </a>
-                </li>
-                <li class="animated fadeInRight animation-delay-2">
-                  <a href="javascript:void(0)" class="btn-circle btn-facebook">
-                    <i class="zmdi zmdi-facebook"></i>
-                  </a>
-                </li>
-                <li class="animated fadeInRight animation-delay-1">
-                  <a href="javascript:void(0)" class="btn-circle btn-twitter">
-                    <i class="zmdi zmdi-twitter"></i>
-                  </a>
-                </li>
-              </ul>
-              <a href="javascript:void(0)" class="btn-circle btn-circle-primary animated zoomInDown animation-delay-7">
-                <i class="zmdi zmdi-share"></i>
-              </a>
-            </div>
-            <a href="javascript:void(0)" class="btn-circle btn-circle-primary no-focus animated zoomInDown animation-delay-8" data-toggle="modal" data-target="#ms-account-modal">
-              <i class="zmdi zmdi-account"></i>
-            </a>
-            <form class="search-form animated zoomInDown animation-delay-9">
-              <input id="search-box" type="text" class="search-input" placeholder="Buscar..." name="q">
-              <label for="search-box">
-                <i class="zmdi zmdi-search"></i>
-              </label>
-            </form>
-            <a href="javascript:void(0)" class="btn-ms-menu btn-circle btn-circle-primary sb-toggle-left animated zoomInDown animation-delay-10">
-              <i class="zmdi zmdi-menu"></i>
-            </a>
-          </div>
+<html>
+<head>
+
+      @include('head')
+</head>
+<body>
+<div class="container">
+
+    <header>jjj
+      @include('header')
+    </header>
+
+    <div id="main" class="row">
+
+        <!-- sidebar content -->
+        <div id="sidebar">
+           @include('sidebar')
         </div>
-      </header>
 
-                </div>
-                <script>
-                        $(document).ready(function(){
-                                function fix_height(){
-                                        var h = $("#tray").height();   
-                                        $("#preview").attr("height", (($(window).height()) - h) + "px");
-                                }
-                                $(window).resize(function(){ fix_height(); }).resize();
-                                //$("#preview").contentWindow.focus();
-                        });
-                </script>
+        <!-- main content -->
+        <div id="content"">
+            
+        </div>
 
-                <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/css/plugins.min.js"></script>
-               <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/css/app.min.js"></script>
-               <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/css/configurator.min.js"></script>
-              <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/css/index.js"></script>
-        </body>
+    </div>
+
+    <footer class="row">
+      
+    </footer>
+
+</div>
+</body>
 </html>
