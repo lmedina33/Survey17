4e8869e7fecc1be2c7dbb85dbf23c7970e8c764e
unknown <luismedina3@hotmail.com>
2017-07-26 07:31:39 -0500

    principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,94 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<!DOCTYPE html>
+<html lang="en-us">
+        <head>
+                <meta charset="utf-8">
+                <title>Encuestas -  CGR - SCI - 2017</title>
+                <meta name="description" content="Live preview for Material Style - Material Design For All at WrapBootstrap">
+                <meta name="viewport" content="width=1024">
+                <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+                <link rel="stylesheet" type="text/css" media="screen" href="//d85wutc1n854v.cloudfront.net/live/css/screen_preview_legacy.css">
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+                <!--<script src="//cdn.optimizely.com/js/233905874.js"></script>-->
+                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+                 <link rel="stylesheet" href="https://agmstudio.io/themes/material-style/1.5.1/assets/css/style.light-blue-500.min.css" />
+                 <link rel="stylesheet" href="https://agmstudio.io/themes/material-style/1.5.1/assets/css/plugins.min.css" />
+                 <link rel="stylesheet" href="https://agmstudio.io/themes/material-style/1.5.1/assets/css/preload.min.css" />
+                 <link rel="stylesheet" href="https://agmstudio.io/themes/material-style/1.5.1/assets/css/width-boxed.min.css" id="ms-boxed" disabled="">
+                <script src="//d85wutc1n854v.cloudfront.net/live/js/behavior_legacy.js"></script>
+                <!--[if lt IE 9]>
+        <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/js/html5shiv.min.js"></script>
+        <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/js/respond.min.js"></script>
+    <![endif]-->
+                
+                <link rel="shortcut icon" href="//d85wutc1n854v.cloudfront.net/live/imgs/favicon.ico">
+        </head>
+        <body>
+                <div id="main">
+                        <header class="ms-header ms-header-primary">
+                                <div class="container container-full">
+          <div class="ms-title">
+            <a href="index.html">
+              <!-- <img src="assets/img/demo/logo-header.png" alt=""> -->
+              <span class="ms-logo animated zoomInDown animation-delay-5">E</span>
+              <h1 class="animated fadeInRight animation-delay-6">Encuesta -
+                <span>CGR-SCI-Comunicaciones</span>
+              </h1>
+            </a>
+          </div>
+          <div class="header-right">
+            <div class="share-menu">
+              <ul class="share-menu-list">
+                <li class="animated fadeInRight animation-delay-3">
+                  <a href="javascript:void(0)" class="btn-circle btn-google">
+                    <i class="zmdi zmdi-google"></i>
+                  </a>
+                </li>
+                <li class="animated fadeInRight animation-delay-2">
+                  <a href="javascript:void(0)" class="btn-circle btn-facebook">
+                    <i class="zmdi zmdi-facebook"></i>
+                  </a>
+                </li>
+                <li class="animated fadeInRight animation-delay-1">
+                  <a href="javascript:void(0)" class="btn-circle btn-twitter">
+                    <i class="zmdi zmdi-twitter"></i>
+                  </a>
+                </li>
+              </ul>
+              <a href="javascript:void(0)" class="btn-circle btn-circle-primary animated zoomInDown animation-delay-7">
+                <i class="zmdi zmdi-share"></i>
+              </a>
             </div>
+            <a href="javascript:void(0)" class="btn-circle btn-circle-primary no-focus animated zoomInDown animation-delay-8" data-toggle="modal" data-target="#ms-account-modal">
+              <i class="zmdi zmdi-account"></i>
+            </a>
+            <form class="search-form animated zoomInDown animation-delay-9">
+              <input id="search-box" type="text" class="search-input" placeholder="Buscar..." name="q">
+              <label for="search-box">
+                <i class="zmdi zmdi-search"></i>
+              </label>
+            </form>
+            <a href="javascript:void(0)" class="btn-ms-menu btn-circle btn-circle-primary sb-toggle-left animated zoomInDown animation-delay-10">
+              <i class="zmdi zmdi-menu"></i>
+            </a>
+          </div>
         </div>
-    </body>
+      </header>
+
+                </div>
+                <script>
+                        $(document).ready(function(){
+                                function fix_height(){
+                                        var h = $("#tray").height();   
+                                        $("#preview").attr("height", (($(window).height()) - h) + "px");
+                                }
+                                $(window).resize(function(){ fix_height(); }).resize();
+                                //$("#preview").contentWindow.focus();
+                        });
+                </script>
+
+                <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/css/plugins.min.js"></script>
+               <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/css/app.min.js"></script>
+               <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/css/configurator.min.js"></script>
+              <script src="https://agmstudio.io/themes/material-style/1.5.1/assets/css/index.js"></script>
+        </body>
 </html>
