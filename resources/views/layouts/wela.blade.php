1256e359efafde7cd60177cdcb95274fa0977ab3
unknown <luismedina3@hotmail.com>
2017-08-07 02:12:17 -0500

    mensaje2

---
+++ resources/views/layouts/wela.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Encuestas - 2017</title>
+<link href="{{ asset('css/bootstrapp.css')}}" rel="stylesheet" type="text/css" media="all">
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="{{ asset('js/jquery-1.11.0.min.js')}}"></script>
+<!-- Custom Theme files -->
+<link href="{{ asset('css/style.css')}}" rel="stylesheet" type="text/css" media="all"/>
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); }>
+</script>
+<meta name="keywords" content="We Photography Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="{{ asset('js/move-top.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/easing.js')}}"></script>
+    <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                $(".scroll").click(function(event){     
+                    event.preventDefault();
+                    $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+                });
+            });
+    </script>
+<!-- //end-smoth-scrolling -->
+<script src="{{ asset('js/bootstrapp.min.js')}}"></script>
+</head>
+<body>
+<!--header start here-->
+<!-- NAVBAR
+        ================================================== -->
+<div class="header">
+    <div class="fixed-header">  
+            <div class="navbar-wrapper">
+              <div class="container">
+                <nav class="navbar navbar-inverse navbar-static-top">
+                     <div class="navbar-header">
+                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                          </button>
+                          <div class="logo wow slideInLeft" data-wow-delay="0.3s">
+                                <a class="navbar-brand" href="index.html"><img src="{{ asset('images/logotipo_2.png')}}" /></a>
+                          </div>
+                      </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                   <nav class="cl-effect-1">    
+                      <ul class="nav navbar-nav">
+                       <li><a href="#" class="scroll"> <span data-hover="Home">Inicio</span></a></li>
+                        <li><a href="#About" class="scroll"> <span data-hover="About">Datos</span></a></li>
+                        <li><a href="#Services" class="
